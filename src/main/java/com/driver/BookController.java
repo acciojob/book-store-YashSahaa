0aabe4c0568b272548645ef7e1b242e53ab851f8
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-02-05 12:57:04 +0530

    Attempt1

--- src/main/java/com/driver/BookController.java
+++ src/main/java/com/driver/BookController.java
@@ -3,10 +3,10 @@ package com.driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("books")
@@ -19,5 +19,52 @@ public class BookController {
     public ResponseEntity createBook(@RequestBody Book book){
         Book newbook = bookService.createBook(book);
         return new ResponseEntity<>(newbook, HttpStatus.CREATED);
+    }
+    @GetMapping("/get-book-by-id/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable String id){
+        Book newbook = bookService.findBookById(id);
+        return new ResponseEntity<>(newbook, HttpStatus.CREATED);
+    }
+
+    // delete request /delete-book-by-id/{id}
+    // pass id as path variable
+    // deleteBookById()
+    @DeleteMapping("/delete-book-by-id/{id}")
+    public ResponseEntity<String> deleteBookById(@PathVariable String id){
+        bookService.deleteBookById(id);
+        return new ResponseEntity<>("Book with id="+ id +" deleted Successfully", HttpStatus.CREATED);
+    }
+
+    // get request /get-all-books
+    // getAllBooks()
+    @GetMapping("/get-all-books")
+    public ResponseEntity<List<Book>> getAllBooks(){
+        List<Book> bookList = bookService.findAllBooks();
+        return new ResponseEntity<>(bookList, HttpStatus.CREATED);
+    }
+
+    // delete request /delete-all-books
+    // deleteAllBooks()
+    @DeleteMapping("/delete-all-books")
+    public ResponseEntity<String> deleteAllBooks(){
+        bookService.deleteAllBooks();
+        return new ResponseEntity<>("Books were deleted successfully", HttpStatus.CREATED);
+    }
+
+    // get request /get-books-by-author
+    // pass author name as request param
+    // getBooksByAuthor()
+    @GetMapping("/get-books-by-author")
+    public ResponseEntity<List<Book>> getBooksByAuthor(@RequestParam String author){
+        List<Book> books = bookService.findBooksByAuthor(author);
+        return new ResponseEntity<>(books, HttpStatus.CREATED);
+    }
+
+    // get request /get-books-by-genre
+    // pass genre name as request param
+    // getBooksByGenre()
+    public ResponseEntity<List<Book>> getBooksByGenre(@RequestParam String genre){
+        List<Book> books = bookService.findBooksByGenre(genre);
+        return new ResponseEntity<>(books, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/driver/BookRepository.java
+++ src/main/java/com/driver/BookRepository.java
@@ -5,37 +5,72 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class BookRepository {
-
+    HashMap<Integer,Book> bookDb ;
+    private int id;
     public BookRepository(){
-        
+        this.bookDb= new HashMap<>();
+        this.id = 1;
     }
 
     public Book save(Book book){
-        return null;
+        book.setId(id);
+        bookDb.put(id,book);
+        id++;
+        return book;
     }
 
     public Book findBookById(int id){
-
-        return null;
+        Book book = null;
+        if(id>0) {
+            for (Book book1 : bookDb.values()) {
+                if (book1.getId() == id) {
+                    book = book1;
+                    break;
+                }
+            }
+        }
+        return book;
     }
 
     public List<Book> findAll(){
-        return null;
+        List<Book> books = new ArrayList<>();
+        for(Book book : bookDb.values()){
+            books.add(book);
+        }
+        return books;
     }
 
     public void deleteBookById(int id){
-        return;
+        if(id<=0) return;
+        for(int i=0;i< bookDb.size();i++){
+            if(bookDb.get(i).getId()==id){
+                bookDb.remove(i);
+                break;
+            }
+        }
     }
 
     public void deleteAll(){
-        return;
+        bookDb.clear();
     }
 
     public List<Book> findBooksByAuthor(String author){
-        return null;
+        List<Book> book = new ArrayList<>();
+        for (Book book1 : bookDb.values()){
+            if(book1.getAuthor().equals(author)){
+                book.add(book1);
+            }
+        }
+        return book;
     }
 
     public List<Book> findBooksByGenre(String genre){
-        return null;
+        List<Book> book = new ArrayList<>();
+        for (Book book1 : bookDb.values()){
+            if(book1.getGenre().equals(genre)){
+                book.add(book1);
+            }
+        }
+        return book;
     }
 }
